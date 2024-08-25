872c7e5ff8f1f2f989b78a06d72b57a2a8c12542
Nguyễn Hoàng Huy <93108281+NguyenHoangHuyProcoderNew@users.noreply.github.com>
2024-08-26 06:49:26 +0700

    Update main.py

--- BOT KEY/main.py
+++ BOT KEY/main.py
@@ -1,5 +1,6 @@
-# IMPORT CÁC THƯ VIỆN CẦN THIẾT
+# Import các thư viện cần thiết
 import os
+from telnetlib import DM
 import time
 import logging
 import threading
@@ -14,53 +15,56 @@ import telebot
 import sys
 import pyperclip
 from selenium.common.exceptions import NoSuchElementException
-logging.basicConfig(level=logging.CRITICAL)  # Chỉ in thông báo lỗi nghiêm trọng
 import datetime
 now = datetime.datetime.now()
 from selenium.common.exceptions import TimeoutException
 
-# KHAI BÁO APT TOKEN BOT TELEGRAM
-API_TOKEN = '6555297922:AAF7DFvu9c-gi10-wBtwa_3jKa3TeyInNQ8'  # TOKEN CỦA BOT
+# Khai báo Token của BOT
+API_TOKEN = '7371036517:AAEB8PtQRtSrvDOxQUUW2su7ObGso6ltq8w' # Token của BOT
 bot = telebot.TeleBot(API_TOKEN)
 
-# CÁC CHỨC NĂNG IN RA MÀN HÌNH
+# Nhập moudle in các log ra màn hình
 from print_logger.print_logger import log_info, log_warning, log_error, log_success
 
-# Nhập chức năng bot phản hồi lại người dùng
+# Nhập moudle bot phản hồi lại người dùng
 from dylib.dylib import bot_reply
 
+# Nhập id và user name của người dùng
 from dylib.dylib import user_id
 from dylib.dylib import username
 
-########################### BẮT ĐẦU CÁC CHỨC NĂNG CỦA BOT ###########################
-log_info("Khởi động bot tạo key thành công, đang đợi lệnh từ người dùng...")
+########## BẮT ĐẦU MÃ CHO CÁC CHỨC NĂNG CỦA BOT #######
+log_success("Khởi động bot tạo key thành công.")
 
 # Tạo key IOS server USER
 @bot.message_handler(commands=['ios_user'])
-def create_key_ios_user(message):
-    from IOS.ios_user import ask_user_timekey_ios_user
-    from IOS.ios_user import main_create_key_ios_user
-    log_info(f"Người dùng {username} đã sử dụng lệnh /ios_user")
-    ask_user_timekey_ios_user(message)
-    bot.register_next_step_handler(message, main_create_key_ios_user)
+def taokey_ios_user(message):
+    # Import các moudle tạo key ios_user
+    from IOS.ios_user import nhap_thoigian_key
+    from IOS.ios_user import xuly_taokey_ios_user
+
+    nhap_thoigian_key(message)
+    bot.register_next_step_handler(message, xuly_taokey_ios_user)
 
 # Tạo key IOS server VIP
 @bot.message_handler(commands=['ios_vip'])
 def ios_vip(message):
-    log_info(f"Người dùng {username} đã sử dụng lệnh /ios_vip")
-    from IOS.ios_vip import ask_user_timekey_ios_vip
-    from IOS.ios_vip import main_create_key_ios_vip
-    ask_user_timekey_ios_vip(message)
-    bot.register_next_step_handler(message, main_create_key_ios_vip)
+    # Import các moudle tạo key ios_vip
+    from IOS.ios_vip import nhap_thoigian_key
+    from IOS.ios_vip import xuly_taokey_ios_vip
+    nhap_thoigian_key(message)
+    bot.register_next_step_handler(message, xuly_taokey_ios_vip)
 
-# CHỨC NĂNG TẠO KEY ANDROID
-from ANDROID.android import ask_user_timekey_android
-from ANDROID.android import main_create_key_android
+# Tạo key ANDROID
 @bot.message_handler(commands=['android'])
-def android(message):
-    log_info(f"Người dùng {username} đã sử dụng lệnh /android")
-    ask_user_timekey_android(message)
-    bot.register_next_step_handler(message, main_create_key_android)
+def taokey_android(message):
+    # Nhập các moudle thực hiện việc tạo key
+    from ANDROID.android import nhap_thoigian_key
+    from ANDROID.android import xuly_taokey_android
+
+    log_info(f"Người dùng đã yêu cầu tạo key android")
+    nhap_thoigian_key(message)
+    bot.register_next_step_handler(message, xuly_taokey_android)
     
 ########################################################
 ####################### CHẠY BOT #######################
